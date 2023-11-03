f9323f7d6fae62e75ff2ee81aaf0dd5e5d8d7215
Jacob Delott <jldelott@gmail.com>
2023-11-02 20:32:15 -0700

    worked on experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -13,61 +13,93 @@ function Experience() {
       <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2010 - 2014"
+          date="2011 - 2013"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            My Random High School, Random Place, Random State
+            Musician's Institute, Hollywood, CA
           </h3>
-          <p> High School Diploma</p>
+          <p> Associates in Bass Performance</p>
+        </VerticalTimelineElement>
+        
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2013 - 2020"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Freelance Bass Player
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Los Angeles, CA
+          </h4>
+          <p>Proffesional touring musician who worked for corporate events and recording artists</p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2014 - 2021"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Digiland Production Facilities, Assistant Facilities Manager
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Burbank, CA
+          </h4>
+          <p>
+            Assisted in maintaining a television production lot.
+          <div>Coordinated vendors, productions, celebritites, and executives in the entertainment industry.</div>
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2020 - 2023"
+          iconStyle={{ background: "#e9d35b", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+           Ovation Auto Detail, Owner
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Burbank, CA
+          </h4>
+          <p>
+            Owned and operated an auto detail business that catered to high end vehicles and high profile professionals and celebrities.
+          </p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2014 - 2018"
+          date="2023"
           iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<SchoolIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            My Cool University, Vancouver, British Columbia
+            Hack Reactor, Los Angeles, CA
           </h3>
-
-          <h4 className="vertical-timeline-element-subtitle">
-            Bachelor's Degree
-          </h4>
-
-          <p> Computer Science</p>
+          <p> Full Stack Engineering Certificate</p>
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          date="2018 - 2020"
+          date="2023- currently"
           iconStyle={{ background: "#e9d35b", color: "#fff" }}
           icon={<WorkIcon />}
         >
           <h3 className="vertical-timeline-element-title">
-            Back End Engineer - Google
+           Software Engineer
           </h3>
           <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>Developed the backend infrastructure for 3 projects.</p>
-        </VerticalTimelineElement>
-
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="2020 - present"
-          iconStyle={{ background: "#e9d35b", color: "#fff" }}
-          icon={<WorkIcon />}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Full Stack Engineer - Twitch
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
+            Los Angeles, CA
           </h4>
           <p>
-            Helped the team launch 2 major features by working both in the front
-            end and back end.
+            <div>Programming Languages | Python 3, JavaScript ES6+, SQL, HTML5, CSS</div>
+            <div>Front-End | DOM manipulation, React, React Hooks, Next.js, Tailwind</div>
+            <div>Back-End | Django 4, PostgreSQL, FastAPI, Docker</div>
+            <div>System Design | Microservices, Domain-driven design, Message passing</div>
+
           </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2> Hi, My Name is Pedro</h2>
+        <h2> Hi, My Name is Jacob</h2>
         <div className="prompt">
           <p>A software developer with a passion for learning and creating.</p>
           <LinkedInIcon />

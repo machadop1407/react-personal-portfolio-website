ea6ea4bd021df5e938abc4707879e364a4fcf17f
Jacob Delott <jldelott@gmail.com>
2023-11-02 19:52:35 -0700

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from "react";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import TwitterIcon from "@material-ui/icons/Twitter";
-import FacebookIcon from "@material-ui/icons/Facebook";
+
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import "../styles/Footer.css";
 
@@ -9,9 +7,7 @@ function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon />
-        <TwitterIcon />
-        <FacebookIcon />
+        
         <LinkedInIcon />
       </div>
       <p> &copy; 2022 pedrotech.com</p>
